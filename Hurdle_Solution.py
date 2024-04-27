90d6c4cf4231295b317b204de20b9864330d9764
Scott L. Quashen <Scottquashen.sq@gmail.com>
2024-04-26 18:10:22 -0700

    Add files via upload

---
+++ Hurdle_Solution.py
@@ -0,0 +1,20 @@
+i = 0
+while i < 6:
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    i+=1
+    
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
